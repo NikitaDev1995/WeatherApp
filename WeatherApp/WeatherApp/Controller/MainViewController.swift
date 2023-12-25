90e9837ac7850228811da1c4ef16968f264e5f33
Nikita Skripka <>
2023-12-25 19:04:13 +1000

    Added UI elements in storyboard

--- WeatherApp/WeatherApp/Controller/MainViewController.swift
+++ WeatherApp/WeatherApp/Controller/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  WeatherApp
 //
 //  Created by Nikita Skripka on 23.12.2023.
@@ -7,35 +7,51 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class MainViewController: UIViewController {
     
     //MARK: - @IBOutlets
-    @IBOutlet weak var mainLabelOutlet: UILabel!
-    @IBOutlet weak var describtionLabelOutlet: UILabel!
-    @IBOutlet weak var speedWindLabelOutlet: UILabel!
-    @IBOutlet weak var temperatureLabelOutlet: UILabel!
-    @IBOutlet weak var feels_likeLabelOutlet: UILabel!
+    @IBOutlet weak var conditionLabelOutlet: UILabel!
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+    @IBOutlet weak var tempLabelOutlet: UILabel!
+    @IBOutlet weak var dateLabelOutlet: UILabel!
+    
+    
+    
+    
+    @IBOutlet weak var collectionViewOutlet: UICollectionView!
     
     //MARK: - Life Scene Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureNavigationController()
         loadData()
+        
+        collectionViewOutlet.delegate = self
+        collectionViewOutlet.dataSource = self
     }
     
     //MARK: - Methods
-    fileprivate func loadData() {
+    private func configureNavigationController() {
+        title = "Ussuriysk"
+        
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        view.backgroundColor = .darkVioletColor
+        dateLabelOutlet.textColor = .grayColor
+        tempLabelOutlet.textColor = .white
+        conditionLabelOutlet.textColor = .grayColor
+    }
+    
+    private func loadData() {
             APIManager.getData(city: "Ussuriysk") { [weak self] result in
                 
                 DispatchQueue.main.async {
                     switch result {
                     case .success(let weather):
                         if let weather {
-                            self?.mainLabelOutlet.text = weather.weather.first?.main
-                            self?.describtionLabelOutlet.text = weather.weather.first?.description
-                            self?.speedWindLabelOutlet.text = "\(weather.wind.speed)"
-                            self?.temperatureLabelOutlet.text = "\(weather.main.temp)"
-                            self?.feels_likeLabelOutlet.text = "\(weather.main.feels_like)"
+                           
                         } else {
                             self?.showError(nil)
                         }
@@ -55,6 +71,25 @@ final class ViewController: UIViewController {
             
             self.present(controller, animated: true)
     }
+    
+    //MARK: - @IBActions
+    @IBAction func refreshBarButtonAction(_ sender: UIBarButtonItem) {
+        
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
 
 //    Optional("{\"coord\":{\"lon\":10.99,\"lat\":44.34},\"weather\":[{\"id\":803,\"main\":\"Clouds\",\"description\":\"broken clouds\",\"icon\":\"04d\"}],\"base\":\"stations\",\"main\":{\"temp\":286.27,\"feels_like\":285.15,\"temp_min\":280.86,\"temp_max\":291.56,\"pressure\":1007,\"humidity\":58,\"sea_level\":1007,\"grnd_level\":923},\"visibility\":10000,\"wind\":{\"speed\":1.7,\"deg\":194,\"gust\":3.63},\"clouds\":{\"all\":56},\"dt\":1703335516,\"sys\":{\"type\":1,\"id\":6812,\"country\":\"IT\",\"sunrise\":1703314173,\"sunset\":1703346009},\"timezone\":3600,\"id\":3163858,\"name\":\"Zocca\",\"cod\":200}")
---
+++ WeatherApp/WeatherApp/Extensions/UIColor + ext/UIColor + ext.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor + ext.swift
+//  WeatherApp
+//
+//  Created by Nikita Skripka on 25.12.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var darkVioletColor: UIColor {
+        UIColor(red: 0.196, green: 0.133, blue: 0525, alpha: 1)
+    }
+    
+    static var lightVioletColor: UIColor {
+        UIColor(red: 0.584, green: 0.490, blue: 0.804, alpha: 1)
+    }
+    
+    static var grayColor: UIColor {
+        UIColor(red: 0.87, green: 0.87, blue: 0.87, alpha: 1)
+    }
+}
